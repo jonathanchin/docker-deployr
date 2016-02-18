a1e6ec9d4d9fe78a0ffee27c219651e8cd3e8baf
minglungweng <minglungweng@iii.org.tw>
2016-02-18 21:23:53 +0800

    MODIFY: reduce step to set public ip

--- startDeployR.sh
+++ startDeployR.sh
@@ -2,18 +2,17 @@
 
 IP_FILE=/opt/expose_ip
 
-/bin/bash -c "${DEPLOYR_HOME}/stopAll.sh"
-sleep 10
-
 if [ ! -z ${EXPOSE_IP} ]; then
     if [ ! -f "${IP_FILE}" ]; then
-        /bin/bash -c "${DEPLOYR_HOME}/startAll.sh"
+
+        /opt/deployr/8.0.0/mongo/mongod.sh start
         sleep 5
         /bin/bash -c "echo y | ${DEPLOYR_HOME}/deployr/tools/setWebContext.sh -ip ${EXPOSE_IP} -disableauto"
         sleep 5
+        /opt/deployr/8.0.0/mongo/mongod.sh stop
+        sleep 5
+
         echo ${EXPOSE_IP} > ${IP_FILE}
-        /bin/bash -c "${DEPLOYR_HOME}/stopAll.sh"
-        sleep 10
         echo "set expose ip to ${EXPOSE_IP}"
     fi
     
