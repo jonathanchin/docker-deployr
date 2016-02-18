48658218241b82b5187f0032f9996f6ab20f8698
minglungweng <minglungweng@iii.org.tw>
2016-02-18 20:54:47 +0800

    MODIFY: add sleep to wait start and stop

--- startDeployR.sh
+++ startDeployR.sh
@@ -2,14 +2,18 @@
 
 IP_FILE=/opt/expose_ip
 
+/bin/bash -c "${DEPLOYR_HOME}/stopAll.sh"
+sleep 10
+
 if [ ! -z ${EXPOSE_IP} ]; then
     if [ ! -f "${IP_FILE}" ]; then
         /bin/bash -c "${DEPLOYR_HOME}/startAll.sh"
+        sleep 5
         /bin/bash -c "echo y | ${DEPLOYR_HOME}/deployr/tools/setWebContext.sh -ip ${EXPOSE_IP} -disableauto"
         sleep 5
         echo ${EXPOSE_IP} > ${IP_FILE}
         /bin/bash -c "${DEPLOYR_HOME}/stopAll.sh"
-        sleep 20
+        sleep 10
         echo "set expose ip to ${EXPOSE_IP}"
     fi
     
